9d5e9682d586cea0e5c0166b9db226466dbb05cf
Christiano Domingues <cristheinz@gmail.com>
2014-04-16 15:01:20 +0100

    titles and new source

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      #"#{base_title} | #{page_title}"
+      page_title
     end
   end
 
